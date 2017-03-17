6bbdeb2c7b20b0304b6793f1afd92cb27a89f462
archit910 <architchauhan910@gmail.com>
2017-03-18 03:17:46 +0530

    This is Database Mongoose

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 var express = require('express');
-var path = require('path');
-// var favicon = require('serve-favicon');
-// var logger = require('morgan');
-var app = express();
-app.use(express.static(path.join(__dirname, 'public')));
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var multer = require('multer');
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var path = require('path');
+var upload = multer(); 
+var app = express();
+app.use(express.static(path.join(__dirname ,'public')));
+app.set('views', __dirname + '/public');
 // app.use(logger('dev'));
 app.set('views', __dirname + '/public');
 app.set('view engine' , 'ejs');
 app.engine('html' , require('ejs').renderFile);
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(upload.array()); // for parsing multipart/form-data
 
 var index = require('./routes/index');
 // var index2 = require('./routes/index2');
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,41 @@
-express = require('express');
-router = express.Router();
+var express = require('express');
+var router = express.Router();
 var path = require('path');
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/minor_db');
 
-router.get('/home' , function(req , res , next){
+router.get('/signup' , function(req , res , next){
 	res.render('index.html');
 });
-router.get('/home2' , function(req , res , next){
-	res.render('test.html');
+var userSchema = mongoose.Schema({
+	name : String,
+	age : Number,
+	country : String,
 });
+var User = mongoose.model("User" , userSchema);
+router.post('/welcome' , function(req , res){
+	var myUser = req.body;
+	// res.send("Added");
+	//res.send(myUser.userName);
+	var newUser = new User({
+		name : myUser.userName,
+		age : myUser.userAge,
+		country : myUser.userNation,
+	});
+	newUser.save(function(req , res){
 
+	});
+	res.render('show_message.html' , {msg : "Thankyou Mr / Mrs " + myUser.userName + " For Sigining Up "})
+});
+router.post('/allusers' , function(req , res){
+	User.find(function(err , response){
+		res.json(response);
+	})
+});
+router.get('/alljson' , function(req , res){
+	User.find(function(err , response){
+		res.json(response);
+	});
+});
 module.exports = router;
